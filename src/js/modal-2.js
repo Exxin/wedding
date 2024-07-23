4d911c33c5983ba31aa3b6bcb3c65c18dc12ebc0
Exxin <exxinblood@gmail.com>
2024-07-23 15:36:34 +0300

    d

--- src/js/modal-2.js
+++ src/js/modal-2.js
@@ -3,10 +3,18 @@
     openModalBtn: document.querySelector('[data-modal-mobile-open]'),
     closeModalBtn: document.querySelector('[data-modal-mobile-close]'),
     modal: document.querySelector('[data-modal-mobile]'),
+    menuLinks: document.querySelector('[nav-link-mobile-close]'),
+    menuLinksOne: document.querySelector('[nav-link-mobile-close-1]'),
+    menuLinksTwo: document.querySelector('[nav-link-mobile-close-2]'),
+    menuLinksThree: document.querySelector('[nav-link-mobile-close-3]'),
   };
 
   refsTwo.openModalBtn.addEventListener('click', toggleModal);
   refsTwo.closeModalBtn.addEventListener('click', toggleModal);
+  refsTwo.menuLinks.addEventListener('click', toggleModal);
+  refsTwo.menuLinksOne.addEventListener('click', toggleModal);
+  refsTwo.menuLinksTwo.addEventListener('click', toggleModal);
+  refsTwo.menuLinksThree.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refsTwo.modal.classList.toggle('is-open-mob');
