b2503480732b62b7da14d13a19d4ca7259db3538
Exxin <exxinblood@gmail.com>
2024-07-23 23:10:52 +0300

    a

---
+++ src/js/slider-2.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function ($) {
+
+  $('#checkbox').change(function(){
+    setInterval(function () {
+        moveRight();
+    }, 4000);
+  });
+
+	var slideCount = $('#slider .collect-list .collect-item').length;
+	var slideWidth = $('#slider .collect-list .collect-item').width();
+	var slideHeight = $('#slider .collect-list .collect-item').height();
+	var sliderUlWidth = slideCount * slideWidth;
+
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+
+	$('#slider .collect-list').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+    $('#slider .collect-list .collect-item:last-child').prependTo('#slider .collect-list');
+
+    function moveLeft() {
+        $('#slider .collect-list').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider .collect-list .collect-item:last-child').prependTo('#slider .collect-list');
+            $('#slider .collect-list').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider .collect-list').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider .collect-list .collect-item:first-child').appendTo('#slider .collect-list');
+            $('#slider .collect-list').css('left', '');
+        });
+    };
+
+    $('a.control-prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control-next').click(function () {
+        moveRight();
+    });
+
+});
