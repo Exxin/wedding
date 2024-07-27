6da538fcacb7f577e492d9175ac58ff0e3bbab35
Exxin <exxinblood@gmail.com>
2024-07-27 21:44:29 +0300

    s

--- src/map.ts
+++ src/map.ts
@@ -5,6 +5,12 @@ async function initMap(): Promise<void> {
     center: { lat: 50.320, lng: 28.753 },
     zoom: 13,
   });
+
+    const marker = new google.maps.Marker({
+    position: { lat: 50.3205, lng: 28.7535 },
+    map: map,
+    title: "Вереси, Житомирська область",
+  });
 }
 
 initMap();
