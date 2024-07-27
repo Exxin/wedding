0cae4c52620bf7057e155e4de0cee13781af0be1
Exxin <exxinblood@gmail.com>
2024-07-27 20:51:52 +0300

    d

--- src/map.ts
+++ src/map.ts
@@ -1,16 +1,10 @@
 let map: google.maps.Map;
 async function initMap(): Promise<void> {
-  const { Map, Marker } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: 50.320, lng: 28.753 },
     zoom: 13,
   });
-
-              const marker = new Marker({
-                position: { lat: 50.320, lng: 28.753 },
-                map: map,
-                title: "Маркер",
-            });
 }
 
 initMap();
