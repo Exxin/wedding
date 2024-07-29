bbf392b490e481a7c89ac0fa73318aa4fe0c33fb
Exxin <exxinblood@gmail.com>
2024-07-29 18:51:52 +0300

    f

--- src/map.ts
+++ src/map.ts
@@ -1,16 +1,24 @@
 let map: google.maps.Map;
+let marker: google.maps.marker.AdvancedMarkerElement;
 async function initMap(): Promise<void> {
   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MapsLibrary;
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: 50.320, lng: 28.753 },
     zoom: 13,
+    mapId: "DEMO_MAP_ID",
   });
 
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-    position: { lat: 50.3205, lng: 28.7535 },
+  const marker = new AdvancedMarkerElement({
+    position: { lat: 50.32074, lng: 28.75360 },
     map,
-    title: "Вереси, Житомирська область",
+    title: "Hello World!",
   });
+  //   new google.maps.Marker({
+  //   position: { lat: 50.32074, lng: 28.75360 },
+  //   map,
+  //   title: "Hello World!",
+  // });
 }
 
 initMap();
