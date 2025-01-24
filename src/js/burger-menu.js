93e5662bd6444a61a1bffef7387bb3dd09991e5b
Bogdan Teteriuk <exxinblood@gmail.com>
2025-01-24 21:46:36 +0200

    fix exit modal

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,6 +1,10 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function () {
+  menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('active');
-  menu.classList.toggle('active');
+  // menu.classList.toggle('active');
 });
+
+
+
+
--- src/js/modal-2.js
+++ src/js/modal-2.js
@@ -1,7 +1,7 @@
 (() => {
   const refsTwo = {
     openModalBtn: document.querySelector('[data-modal-mobile-open]'),
-    closeModalBtn: document.querySelector('[data-modal-mobile-close]'),
+    // closeModalBtn: document.querySelector('[data-modal-mobile-close]'),
     modal: document.querySelector('[data-modal-mobile]'),
     menuLinks: document.querySelector('[nav-link-mobile-close]'),
     menuLinksOne: document.querySelector('[nav-link-mobile-close-1]'),
@@ -11,12 +11,12 @@
   };
 
   refsTwo.openModalBtn.addEventListener('click', toggleModal);
-  refsTwo.closeModalBtn.addEventListener('click', toggleModal);
+  // refsTwo.closeModalBtn.addEventListener('click', toggleModal);
   refsTwo.menuLinks.addEventListener('click', toggleModal);
   refsTwo.menuLinksOne.addEventListener('click', toggleModal);
   refsTwo.menuLinksTwo.addEventListener('click', toggleModal);
   refsTwo.menuLinksThree.addEventListener('click', toggleModal);
-  refsTwo.menuLinksFour.addEventListener('click', toggleModal);
+  // refsTwo.menuLinksFour.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refsTwo.modal.classList.toggle('is-open-mob');
