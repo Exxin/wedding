74747b36b5b810ba7230c24c2e060fe9aea17180
Bogdan Teteriuk <exxinblood@gmail.com>
2025-01-27 11:50:24 +0200

    modified mobile modal in header

--- src/js/modal-2.js
+++ src/js/modal-2.js
@@ -1,29 +1,26 @@
 (() => {
   const refsTwo = {
     openModalBtn: document.querySelector('[data-modal-mobile-open]'),
-    // closeModalBtn: document.querySelector('[data-modal-mobile-close]'),
     modal: document.querySelector('[data-modal-mobile]'),
     menuLinks: document.querySelector('[nav-link-mobile-close]'),
     menuLinksOne: document.querySelector('[nav-link-mobile-close-1]'),
     menuLinksTwo: document.querySelector('[nav-link-mobile-close-2]'),
     menuLinksThree: document.querySelector('[nav-link-mobile-close-3]'),
     menuLinksFour: document.querySelector('[nav-link-mobile-close-4]'),
+
+    headerContainer: document.querySelector('.header-container'),
   };
 
   refsTwo.openModalBtn.addEventListener('click', toggleModal);
-  // refsTwo.closeModalBtn.addEventListener('click', toggleModal);
   refsTwo.menuLinks.addEventListener('click', toggleModal);
   refsTwo.menuLinksOne.addEventListener('click', toggleModal);
   refsTwo.menuLinksTwo.addEventListener('click', toggleModal);
   refsTwo.menuLinksThree.addEventListener('click', toggleModal);
-  // refsTwo.menuLinksFour.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refsTwo.modal.classList.toggle('is-open-mob');
+    refsTwo.headerContainer.classList.toggle('blur');
   }
 })();
-
-const test = document.querySelector(".mob-menu-item");
-
 // ============================================================
 // ============================================================
